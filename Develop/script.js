5bb15f239903090215933c479402b73a030c925e
daniel.thomas215@hotmail.com <daniel.thomas215@hotmail.com>
2022-05-19 12:01:14 -0400

    random password update

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,24 @@
 // Assignment code here
+function generatePassword(){
+ 
+var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+var randomPassword = upperCase[Math.floor(Math.random() * upperCase.length)]
+ 
+return randomPassword
+}
 
+//generatePassword()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  //generatePassword = 7;
 
 }
 
