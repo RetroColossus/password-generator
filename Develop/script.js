7f92976d9aa1b4d9db52c155e1b9d706e8703f29
daniel.thomas215@hotmail.com <daniel.thomas215@hotmail.com>
2022-05-19 14:50:10 -0400

    added special character  option

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,23 @@
 // Assignment code here
 function generatePassword(){
-  var userInput = window.prompt("How many characters between 8-128?");
+  var userInput1 = window.prompt("please choose password length between 8-128");
+  var userInput2 = window.prompt("Would you like special characters?");
   var randomAllPw = "";
-  var  randomAll= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*";
-  
-    if (userInput < 8 || userInput > 128){
+  var randomAll= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*";
+  var specialCharUpperCase = "!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialCharLowerCase = "!@#$%^&*abcdefghijklmnopqrstuvwxyz";
+
+    if (userInput1 < 8 || userInput1 > 128){
       randomAll = window.alert("try again");
+    } else if (userInput1 > 8 || userInput1 < 8) {
+      for (var i = 0; i < userInput1; i++)
+    randomAllPw += randomAll.charAt(Math.floor(Math.random() * randomAll.length));
+    } else if (userInput2 === "yes" || userInput2 === Yes) {
+      for (var i = 0; i < userInput2; i++)
+    randomAllPw += randomAll.charAt(Math.floor(Math.random() * randomAll.length));
     }
 
-  for (var i = 0; i < userInput; i++)
+  for (var i = 0; i < userInput1; i++)
     randomAllPw += randomAll.charAt(Math.floor(Math.random() * randomAll.length));
 
   return randomAllPw;
